7467590a595b2b6a53c3b96091f00bc548d45206
Craig M. Stimmel <cstimmel@uw.edu>
2013-07-04 14:15:28 -0700

    Calculated each project's hours as a percentage of a 40 hour week.

--- uwit_ax_parser.py
+++ uwit_ax_parser.py
@@ -5,6 +5,7 @@ from xml.dom import minidom
 
 #TODO: make this take the timesheet file as an argument, not as a hard-coded var
 TIMESHEET = "/Users/cstimmel/Downloads/TSTimesheetSignOff.Report.xml"
+WEEK_HOURS = 40  # The number of hours that should be used to calculate each project as a percentage of the week's work.
 
 
 def main(timesheet):
@@ -12,9 +13,9 @@ def main(timesheet):
     details = xmldoc.getElementsByTagName('Detail')
     for detail in details:
         project = detail.getAttribute('Textbox_59')
-        totalhrs = detail.getAttribute('TotalHours1')
-        print project
-        print totalhrs
+        totalhrs = float(detail.getAttribute('TotalHours1'))
+        percentage = totalhrs/WEEK_HOURS * 100
+        print "%s: %s%% (%s)" % (project, percentage, totalhrs)
 
 
 if __name__ == "__main__":
