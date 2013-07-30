c5eb22b79145bbf7d7dc833af80f9f68786aecde
Stephy Ma <stephyma@uw.edu>
2013-07-30 05:13:37 -0700

    added debug option, output log and filter details

--- read-file.py
+++ read-file.py
@@ -4,71 +4,61 @@ import urllib
 import pdb
 import argparse
 
+DEBUG = True;
+
 def main(args):
     if args.filename:
     
         f = gzip.open(args.filename)
-        #f = gzip.open('access_log.20130529.gz')
-        #newfile = open('result.txt', 'w')
-        #newfile2 = open('phrases.txt', 'w')
         regex = re.compile('\/search\/\?')
         count = 0
         pCount = 0
         dictionary = {}
             
-            
         for line in f:
             search_terms = re.search(regex, line)
             if search_terms:
-         #       pdb.set_trace()
-        
                 count = count + 1
-         #       print count, line    #print the entire log line
-                
+                if DEBUG == True: 
+                    print "Entry #%d: " % count
+                    print "Original log entry: %s" % line  #print the entire log line
                 line = urllib.unquote(line) #decode URL
-         #       print line
-         #       newfile.write(line)
-            
-            
-                pCount = 0
-                phrases = line.split('&')   #split the line by different request components
-               # print phrases[0]
-                firstLine = phrases[0].split('?')   #strip the stuff before question mark
-               # print firstLine[1]
+                if DEBUG == True:
+                    print "Original log entry with decoded url: %s" % line
+                
+                phrases = line.split('&')   #split the line by different filters
+                firstLine = phrases[0].split('?')   #strip the stuff before question mark that's attached to the filter
                 phrases[0] = firstLine[1]
             
                 length = len(phrases)
-               # print phrases[length - 1]
-                lastLine = phrases[length - 1].split('HTTP')
-               # print lastLine[0]
+                lastLine = phrases[length - 1].split('HTTP')  #strip the unnecessary stuff attached to the last filter
                 phrases[length - 1] = lastLine[0]
+                
+                pCount = 0
                 for phrase in phrases:
                     pCount = pCount + 1
-         #           print pCount, phrase
+                    if DEBUG == True:
+                        print "Filter #%d: %s" % (pCount, phrase)
                     request = phrase.split('=')
                     
-                    key = request[0]
-                   # print request[0]
-                    innerKey = request[1]
-                   # print request[1]
+                    key = request[0]        #outer dict keys(filters) are text to the left of equal sign 
+                    innerKey = request[1]   #inner dict keys(corresponding options) are text to the right of equal sign
         
                     if key.startswith('extended_info'):
-                        key = key[14:]
+                        key = key[14:]      #strip 'extended_info' from key
                     
-                    if key in dictionary:
+                    if key in dictionary:   #if key(filter) already exists
                         if innerKey in dictionary[key]:
-                            dictionary[key][innerKey] += 1
-         #                   print "innerkey exists: " + str(dictionary[key][innerKey])
+                            dictionary[key][innerKey] += 1   #increment of the option frequency
                         else:
-                            dictionary[key][innerKey] = 1
-         #                   print "create new innerKey: " + str(dictionary[key][innerKey])
-                    else:
-                        
-                        dictionary[key] = {innerKey: 1}
-         #               print "create new key: " + str(dictionary[key][innerKey])
+                            dictionary[key][innerKey] = 1    #create new mapping and set the value(frequency of a certain option) to 1
+                    else:                   #key(filter) not exist
+                        dictionary[key] = {innerKey: 1}      #create new mapping --- inner dict key:option; inner dict value: frequency(1); outer dict key: filter term; outer dict value: inner dict
         
-         #       print '\n'
+                if DEBUG == True: 
+                    print '\n'
         
+        #delete the following because they are not very useful?
         if 'center_latitude' in dictionary:
             del dictionary['center_latitude']
         if 'center_longitude' in dictionary:
@@ -78,8 +68,10 @@ def main(args):
         if 'limit' in dictionary:
             del dictionary['limit']
         
-        print dictionary
-        print 'Total search found:', count
+        if DEBUG == True:
+            print dictionary
+            print
+        print "Total search found: %d" % count + '\n'
         
         print "Search Summary:" + '\n'
         for keys in dictionary:
