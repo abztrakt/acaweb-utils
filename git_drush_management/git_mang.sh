02731292c60b472ab4e95d94fe984e2fb87c0bb7
Matthew Muiter <muiter@uw.edu>
2014-06-06 12:59:40 -0700

    added drush commands to take drupal site offline and online

--- git_drush_management/git_mang.sh
+++ git_drush_management/git_mang.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
 NOW=$(date +"%m-%d-%y-%H-%M-%S")
+
+#echo -n "Enter a command for which directory you want to save the SQL in (e.g. ~) "
+#read sql_location
 
 cd ~
 cd public_html/drupal/
-drush sql-dump --result-file=../$NOW.sql
+#cd $sql_location
+
+drush sql-dump --result-file=../18.sql
+
+drush vset --exact site_offline 1
+
+#git fetch --tags
+
+#git checkout -b $branch $tag
+
+drush vset --exact site_offline 0
