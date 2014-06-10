afcdfb78e21ce1c6b7d49dae0bab7d05f1a84239
Matthew Muiter <muiter@uw.edu>
2014-06-10 14:26:48 -0700

    git_mang.sh reads from variable_list.sh to get the variables

--- git_drush_management/git_mang.sh
+++ git_drush_management/git_mang.sh
@@ -2,37 +2,35 @@
 
 NOW=$(date +"%m-%d-%y-%H-%M-%S")
 
+source variable_list.sh
 cd ~
-echo "We are in your home directory now"
-echo -n "Enter the location to cd into your Druapl directory (e.g. public_html/drupal) "
-read drupal_location
-cd $drupal_location
-echo `pwd`
-echo "We're in!"
+#echo "We are in your home directory now"
+cd $DRUPAL_LOCATION
+#echo `pwd`
+#echo "We're in!"
 
 drush sql-dump --result-file=../$NOW.sql
 
-echo -n "Enter the location for which directory you want to move the SQL file into (e.g. testing/) "
-read sql_location
-
-#this goes above drupal site 
+#this goes above drupal site to where .sql was just saved 
 cd ..
+#echo This is your current directory: `pwd`
 
 #check if sql file is in there
 #then move file to $sql_location
-mv $NOW.sql $sql_location
+mv $NOW.sql $SQL_LOCATION
 
-#do i need to cd back into ~ then back into $drupal_location instead of command below
+cd ~
+cd $DRUPAL_LOCATION
 
 drush vset --exact site_offline 1
 
 echo -n "Enter a name for the branch you want to create (e.g. eScience3.6) "
-read branch
+read BRANCH_NAME
 echo -n "Enter a name for the tag you want to create (e.g. v3.6) "
-read tag
+read TAG_NAME
 
 git fetch --tags
 
-git checkout -b $branch $tag
+git checkout -b $BRANCH_NAME  $TAG_NAME
 
 drush vset --exact site_offline 0
---
+++ git_drush_management/variable_list.sh
@@ -0,0 +1,11 @@
+# directory to Drupal location from ~
+DRUPAL_LOCATION="public_html/drupal"
+
+# enter directory to save SQL file from ../drupal directory
+SQL_LOCATION="testing/"
+
+#branch name
+#BRANCH="escience3.6"
+
+#tag name
+#TAG="v3."6
