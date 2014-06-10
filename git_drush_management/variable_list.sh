c742e2ca63894969ff6deb561cdc721873d27d12
Matthew Muiter <muiter@uw.edu>
2014-06-10 14:33:02 -0700

    either add git branch and tag variables in variable_sh or git_mang script

--- git_drush_management/variable_list.sh
+++ git_drush_management/variable_list.sh
@@ -4,8 +4,10 @@ DRUPAL_LOCATION="public_html/drupal"
 # enter directory to save SQL file from ../drupal directory
 SQL_LOCATION="testing/"
 
+#CAN EITHER SET THIS HERE OR IN git_mang.sh
 #branch name
 #BRANCH="escience3.6"
 
+#CAN EITHER SET THIS HERE OR IN git_mang.sh
 #tag name
 #TAG="v3."6
