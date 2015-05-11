ab41ea1b486d45e81c4211a26960386e735df53d
Kaiyu Zheng <qaz2wsx3@uw.edu>
2015-05-11 16:30:18 -0700

    Some javascript to make it better. It is not very stylish javascript.

--- wp_json/demo.php
+++ wp_json/demo.php
@@ -6,8 +6,22 @@
     <style>
      input[type="text"] {
        margin:5px;
+       font-size:17px;
+       height:22px;
+       border:1px solid black;
+     }
+
+     input[type="submit"] {
+       padding:5px;
+       font-size:17px;
+       background-color:#DDE;
+     }
+
+     #real-url {
+       font-weight:700;
      }
     </style>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
   </head>
   <body>
   </body>
@@ -61,7 +75,7 @@ if (is_null($username)) {
 
     // Check submit form
     if (isset($_POST['submit_input'])) {
-        $request_url = $_POST['url_input'];
+        $request_url = $_POST['url_request'];
         $wp_http = new WP_Http;
 
         $result = $wp_http->request ($request_url, $args);
@@ -79,13 +93,82 @@ function get_form() {
   <form name="input_form" id="input-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <input type="text" placeholder="Your base url. E.g. http://wonka.cac.washington.edu/itconnect/wp-json" name="url_base" id="url-base" size="80"><br>
     <!--input type="text" placeholder="Enter a url here" name="url_input" id="url-input" size="100"-->
-    <input type="radio" name="type" value="post" checked>post<input type="text" placeholder="post id" name="post_id" style="display:none" id="post-id" size="20"><br>
-    <input type="radio" name="type" value="page">page<input type="text" placeholder="page id" name="page_id" style="display:none" id="page-id" size="20"><br>
-    <input type="radio" name="type" value="custom">custom<input type="text" placeholder="custom type id" name="custom_id" style="display:none" id="custom-id" size="20"><br>
-    <input type=text" placeholder="custom type" name="custom_type" style="display:none" id="custom-type" size="40">
-    <p id="url-to-request">You are about to request <b></b></p>
+    <input type="radio" name="type" value="post" id="post-radio-btn" checked>post<input type="text" placeholder="post id" name="post_id" style="display:none" id="post-id" size="20"><br>
+    <input type="radio" name="type" value="page" id="page-radio-btn">page<input type="text" placeholder="page id" name="page_id" style="display:none" id="page-id" size="20"><br>
+    <input type="radio" name="type" value="custom" id="custom-radio-btn">custom<input type="text" placeholder="custom type id" name="custom_id" style="display:none" id="custom-id" size="20"><br>
+    <input type="text" placeholder="custom type" name="custom_type" style="display:none" id="custom-type" size="40">
+    <hr>
+    <input type="checkbox" name="meta" id="meta-checkbox">meta
+    <p id="url-to-request">You are about to request <input type="text" name="url_request" value="" size="100" id="real-url"></p>
     <input type="submit" value="Submit" id="submit-input" name="submit_input" class="submit-btn">
   </form>
+  <script>
+   var id = "";
+   var type = "";
+
+   $(document).ready(function(){
+       checkTypeRadioStatus();
+       checkMetaCheckbox();
+
+       $('input[name="type"]').click(function() {
+           checkTypeRadioStatus();
+       });
+       $('#meta-checkbox').click(function() {
+           checkMetaCheckbox();
+       });
+
+       $('#url-base').change(function() {
+           checkMetaCheckbox();
+       });
+       $('#post-id').change(function() {
+           id = $('#post-id').val();
+           checkMetaCheckbox();
+       });
+       $('#page-id').change(function() {
+           id = $('#page-id').val();
+           checkMetaCheckbox();
+       });
+       $('#custom-id').change(function() {
+           id = $('#custom-id').val();
+           checkMetaCheckbox();
+       });
+   });
+
+   function checkTypeRadioStatus() {
+       if ($('#post-radio-btn').is(':checked')) {
+           type = "post";
+           $('#post-id').css('display', 'inline');
+           checkMetaCheckbox();
+       } else {
+           $('#post-id').css('display', 'none');
+       }
+       if ($('#page-radio-btn').is(':checked')) {
+           type = "page";
+           $('#page-id').css('display', 'inline');
+           checkMetaCheckbox();
+       } else {
+           $('#page-id').css('display', 'none');
+       }
+       if ($('#custom-radio-btn').is(':checked')) {
+           type = "";
+           $('#custom-id').css('display', 'inline');
+           $('#custom-type').css('display', 'inline');
+           checkMetaCheckbox();
+       } else {
+           $('#custom-id').css('display', 'none');
+           $('#custom-type').css('display', 'none');
+       }
+   }
+
+   // changes the url, according to whether of not the meta checkbox is checked
+   function checkMetaCheckbox() {
+       if ($('#meta-checkbox').is(':checked')) {
+           $('#url-to-request input').val($('#url-base').val() + "/" + type + "/" + id + '/meta');
+       } else {
+           $('#url-to-request input').val($('#url-base').val() + "/" + type + "/" + id);
+       }
+   }
+  </script>
 <?php
 }
 
