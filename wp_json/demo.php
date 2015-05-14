39777f8bc4f600c26fe1a223237b9a3a47d183e3
Kaiyu Zheng <qaz2wsx3@uw.edu>
2015-05-14 12:28:12 -0700

    This is pretty much it for now. Adding json format function written by others

--- wp_json/demo.php
+++ wp_json/demo.php
@@ -22,7 +22,6 @@
      }
 
      .json-area {
-       width:1000px; 
        background-color:#FFFFCC; 
        color:#000;
        font-size:13px;
@@ -95,11 +94,80 @@ if (is_null($username)) {
         echo "<hr>";
         echo "You requested: <b>$request_url</b> <br>";
         echo "Go to <a href='http://json.parser.online.fr' target='_blank'> JSON Parser Online</a> to simply format the JSON";
-        put_json($result['body']);
+        put_json(format_json($result['body']));
 
         // unset because we are done
         unset($_POST['submit_input']);
     }
+}
+
+function format_json($json, $html = false, $tabspaces = null)
+{
+    $tabcount = 0;
+    $result = '';
+    $inquote = false;
+    $ignorenext = false;
+
+    if ($html) {
+        $tab = str_repeat("&nbsp;", ($tabspaces == null ? 4 : $tabspaces));
+        $newline = "<br/>";
+    } else {
+        $tab = ($tabspaces == null ? "\t" : str_repeat(" ", $tabspaces));
+        $newline = "\n";
+    }
+
+    for($i = 0; $i < strlen($json); $i++) {
+        $char = $json[$i];
+
+        if ($ignorenext) {
+            $result .= $char;
+            $ignorenext = false;
+        } else {
+            switch($char) {
+                case ':':
+                    $result .= $char . (!$inquote ? " " : "");
+                    break;
+                case '{':
+                    if (!$inquote) {
+                        $tabcount++;
+                        $result .= $char . $newline . str_repeat($tab, $tabcount);
+                    }
+                    else {
+                        $result .= $char;
+                    }
+                    break;
+                case '}':
+                    if (!$inquote) {
+                        $tabcount--;
+                        $result = trim($result) . $newline . str_repeat($tab, $tabcount) . $char;
+                    }
+                    else {
+                        $result .= $char;
+                    }
+                    break;
+                case ',':
+                    if (!$inquote) {
+                        $result .= $char . $newline . str_repeat($tab, $tabcount);
+                    }
+                    else {
+                        $result .= $char;
+                    }
+                    break;
+                case '"':
+                    $inquote = !$inquote;
+                    $result .= $char;
+                    break;
+                case '\\':
+                    if ($inquote) $ignorenext = true;
+                    $result .= $char;
+                    break;
+                default:
+                    $result .= $char;
+            }
+        }
+    }
+
+    return $result;
 }
 
 function get_form() {
@@ -141,5 +209,4 @@ function put_json($str) {
   </div>
 <?php
 }
-
 ?>
